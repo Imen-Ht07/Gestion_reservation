2d5a9a3f8e6f914745f4b979ff93ad14a5959355
Hadj Taieb Imen <115839915+Imen-Ht07@users.noreply.github.com>
2025-04-23 12:24:23 +0200

    auth

--- reservation_backend/reservation_service/reservationController.js
+++ reservation_backend/reservation_service/reservationController.js
@@ -1,4 +1,8 @@
-const { Reservation} = require('./reservation');
+const Reservation = require('./reservation');
+const { publishReservationEvent } = require('./producerKafka');
+const axios = require('axios');
+const SALLE_SERVICE_URL = process.env.SALLE_SERVICE_URL || 'http://localhost:3004';
+const USER_SERVICE_URL = process.env.USER_SERVICE_URL || 'http://localhost:3001';
 
 const getAllReservations = async (req, res) => {
     try {
@@ -8,11 +12,10 @@ const getAllReservations = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
-
 const createReservation = async (req, res) => {
     try {
-        const { salle_id, date_heure_debut, date_heure_fin } = req.body;
-        
+        const { utilisateur_id, salle_id, date_heure_debut, date_heure_fin } = req.body;
+
         // Vérification des conflits de réservation
         const conflit = await Reservation.findOne({
             salle_id,
@@ -24,11 +27,40 @@ const createReservation = async (req, res) => {
         if (conflit) {
             return res.status(400).json({ message: 'Salle déjà réservée pour ce créneau' });
         }
-        
-        const nouvelleReservation = new Reservation(req.body);
+
+        // Création de la réservation
+        const nouvelleReservation = new Reservation({
+            utilisateur_id,
+            salle_id,
+            date_heure_debut,
+            date_heure_fin
+        });
+
         await nouvelleReservation.save();
+
+        //  Récupérer les détails utilisateur et salle pour l'event Kafka
+        const salleResponse = await axios.get(`${SALLE_SERVICE_URL}/api/salles/${salle_id}`);
+        const salle = salleResponse.data;
+        const utilisateurResponse = await axios.get(`${USER_SERVICE_URL}/api/users/${utilisateur_id}`);
+        const utilisateur = utilisateurResponse.data;
+
+        if (utilisateur && salle) {
+            await publishReservationEvent({
+                utilisateur_id: {
+                    email: utilisateur.email,
+                    nom: utilisateur.nom
+                },
+                salle_id: {
+                    nom: salle.nom
+                },
+                date_heure_debut: nouvelleReservation.date_heure_debut,
+                date_heure_fin: nouvelleReservation.date_heure_fin
+            });
+        }
+
         res.status(201).json(nouvelleReservation);
     } catch (error) {
+        console.error('Erreur lors de la création de la réservation :', error.message);
         res.status(400).json({ message: error.message });
     }
 };
